b0201f1cc34abb4d987bc48e7ac0c6e9b7f887c6
Phuc Dang <danglephuc@gmail.com>
2020-08-04 22:18:20 +0700

    Add queue

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const path = require('path');
 const lineReader = require('line-reader');
 require('dotenv').config();
 var AWS = require('aws-sdk');
+var Queue = require('better-queue');
 
 // Create an S3 client
 var s3 = new AWS.S3();
@@ -9,43 +10,38 @@ var bucketName = 'anlab-lashinbang';
 var imageFolder = 'images';
 var imageNewFolder = 'images-update';
 var count = 0;
-var thread = process.argv[2];
 var countProcess = 0;
 var countDone = 0;
 
-lineReader.eachLine(path.resolve(__dirname, process.env.UPDATE_FILE), function(line) {
-    let files = line.split(',');
+var q = new Queue(function (input, cb) {
+    let files = input.split(',');
     count++;
 
-    if(count % 16 == thread) {
-        let srcFile = imageFolder + '/' + files[0];
-        let dstFile = imageNewFolder + '/' + files[1];
+    let srcFile = imageFolder + '/' + files[0];
+    let dstFile = imageNewFolder + '/' + files[1];
 
-        var params = {
-            Bucket: bucketName, 
-            Key: dstFile,
-            CopySource: bucketName + '/' + srcFile,
-            ACL: 'public-read',
-            CacheControl: 'public, max-age=31536000'
-        };
-        countProcess++;
-        s3.copyObject(params, function(err, data) {
-            countDone++;
-            if(countDone % 100 == 0) {
-                console.log("Count: " + count);
-            }
-            if (err)
-                console.log("Error: " + srcFile);
-            // else
-            //     console.log("Successfully uploaded data to " + bucketName + "/" + dstFile);
-        });
-    }
+    var params = {
+        Bucket: bucketName, 
+        Key: dstFile,
+        CopySource: bucketName + '/' + srcFile,
+        ACL: 'public-read',
+        CacheControl: 'public, max-age=31536000'
+    };
+    countProcess++;
+    s3.copyObject(params, function(err, data) {
+        countDone++;
+        // if(countDone % 100 == 0) {
+            console.log("Count: " + countDone);
+        // }
+        if (err)
+            console.log("Error: " + srcFile);
+        // else
+        //     console.log("Successfully uploaded data to " + bucketName + "/" + dstFile);
+    });
+   
+    cb(null, result);
+  }, {concurrent: 20});
+
+lineReader.eachLine(path.resolve(__dirname, process.env.UPDATE_FILE), function(line) {
+    q.push(line);
 });
-
-while(true) {
-    if(countDone === countProcess) {
-        break;
-    }
-}
-
-console.log('Exit');
